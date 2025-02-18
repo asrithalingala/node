a7d75487c135b94732e80e76fa3eb6638ac57ca8
asrithalingala <alingala@hawk.iit.edu>
2025-02-18 13:42:58 -0600

    Added route for updating employee details

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -7,4 +7,6 @@ const Employee = require("../models/Employee")
 router.post('/add-emp', employeeController.createEmployee)
 router.get('/allemployees', employeeController.getEmployees)
 router.get('/employee/:id', employeeController.singleEmployee)
+router.put('/update/:id', employeeController.updateEmployee)
+
 module.exports = router
