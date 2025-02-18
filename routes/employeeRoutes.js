2ce57deffa1dbe4dabb45056c5a339f49d614b0c
asrithalingala <alingala@hawk.iit.edu>
2025-02-18 13:00:19 -0600

    Implemented employee creation endpoint

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const employeeController = require("../controllers/employeeController")
+const Employee = require("../models/Employee")
+
+
+router.post('/add-emp', employeeController.createEmployee)
+router.get('/allemployees', employeeController.getEmployees)
+
+module.exports = router
