bc51aba34f01420c1dc54bae379c88b5c9bbf70e
asrithalingala <alingala@hawk.iit.edu>
2025-02-20 13:50:35 -0600

    modified

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,77 +1,80 @@
-const Employee = require("../models/Employee")
-const createEmployee = async(req,res)=>{
-    try{
-        const {name, email, phone, city} = req.body
-        const employee = new Employee({
-            name,
-            email,
-            phone,
-            city
-        })
-       await employee.save()
-       res.status(201).json(employee)
+const Employee = require("../models/Employee");
 
-    }catch(error) {
-        console.log("there is an error:",  error)
-        res.status(500).json({message : 'Server error'})
+const createEmployee = async (req, res) => {
+  try {
+    const { name, email, phone, city } = req.body;
+    const employee = new Employee({
+      name,
+      email,
+      phone,
+      city,
+    });
+    await employee.save();
+    res.status(201).json(employee);
+  } catch (error) {
+    console.log("There is an error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    console.error("There is an error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const singleEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      return res.status(404).json({ message: "Employee not found" });
     }
-}
-const getEmployees = async(req,res)=>
-{
-    try{
-        const employees = await Employee.find()
-        res.status(200).json(employees)
-    }catch(error){
-        console.error("There is an error:", error)
-        res.status(500).json({message: 'server error'})
+    res.status(200).json(employee);
+  } catch (error) {
+    console.error("There is an error", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const updateEmployee = async (req, res) => {
+  try {
+    const { name, email, phone, city } = req.body;
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+      req.params.id,
+      { name, email, phone, city },
+      { new: true }
+    );
+    if (!updatedEmployee) {
+      return res.status(404).json({ message: "Employee not found" });
     }
-}
-const singleEmployee = async(req, res)=>{
-    try{
-        const employee = await Employee.findById(req.params.id)
-        if(!employee){
-            return res.status(404).json({message: "Employee not found"})
-        }
-        res.status(200).json(employee)
+    res.status(200).json(updatedEmployee);
+  } catch (error) {
+    console.error("There is an error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteEmployee = async (req, res) => {
+  try {
+    const deletedEmployee = await Employee.findByIdAndDelete(req.params.id);
+    if (!deletedEmployee) {
+      return res.status(404).json({ message: "Employee not found" });
     }
-    catch(error){
-        console.error("there is an error", error)
-        res.status(500).json({message: "server error"})
+    res.status(204).send();
+  } catch (error) {
+    console.error("There is an error", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-    }
-
-}
-const updateEmployee = async(req, res)=>{
-    try{
-        const {name,email, phone, city} = req.body
-        
-
-        const myEmployee = await Employee.findByIdAndUpdate(
-            req.params.id,
-            {name,email,phone,city}
-        )
-        if(!myEmployee){
-            return res.status(404).json({message : "employee not found"})
-
-        }
-        res.status(200).json(myEmployee)
-    }
-    catch(error){
-        console.error('there is an error:', error)
-        res.status(500).json({message: "Server error"})
-        }
-}
-const deleteEmployee = async(req, res)=>{
-    try{
-        const deleteemployee = await Employee.findByIdAndDelete(req.params.id)
-        res.status(204).send()    
-    }
-    catch(error){
-        console.error("there is an error", error)
-        res.status(500).json({message: "server error"})
-
-    }
-
-}
-
-module.exports = {createEmployee, getEmployees, singleEmployee, updateEmployee, deleteEmployee}
+module.exports = {
+  createEmployee,
+  getEmployees,
+  singleEmployee,
+  updateEmployee,
+  deleteEmployee,
+};
